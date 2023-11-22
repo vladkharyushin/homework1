58878a9ec89a4e610e0f6c170fad7aed75131845
vladkharyushin <vx3110@yandex.ru>
2023-11-22 18:16:08 +0300

    Commit - 3

--- src/settings.ts
+++ src/settings.ts
@@ -172,6 +172,14 @@ app.put('/videos/:id', (req: RequestWithBodyAndParams<Params, UpdateVideoTo>, re
     } else {
         minAgeRestriction = null
     }
+
+    const isoDateReqEx = /^[0-9]{4}-((0[13578]|1[02])-(0[1-9]|[12][0-9]|3[01])|(0[469]|11)-(0[1-9]|[12][0-9]|30)|(02)-(0[1-9]|[12][0-9]))T(0[0-9]|1[0-9]|2[0-3]):(0[0-9]|[1-5][0-9]):(0[0-9]|[1-5][0-9])\.[0-9]{3}Z$/
+    if (typeof publicationDate !== "undefined" &&  !isoDateReqEx.test(publicationDate)) {
+        error.errorsMessages.push({
+            message: "Invalid publicationDate",
+            field: "publicationDate"})
+    }
+
     if (error.errorsMessages.length) {
         res.status(400).send(error)
         return
