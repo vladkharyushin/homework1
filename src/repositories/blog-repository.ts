1fcb5929695f1cec41c9d59c5344ff2378717c10
vladkharyushin <vx3110@yandex.ru>
2023-11-29 17:44:09 +0300

    Repositories change

--- src/repositories/blog-repository.ts
+++ src/repositories/blog-repository.ts
@@ -1,32 +1,66 @@
-import {db} from "../db/db";
-import {BlogType} from "../types/blog/output";
+import {BlogType, OutputBlogType} from "../types/blog/output";
+import {blogCollection} from "../db/db";
+import {ObjectId} from "mongodb";
+import {blogMapper} from "../types/blog/blog-mapper";
+import {InputBlogType, UpdateBlogData} from "../types/blog/input";
 
 export class BlogRepository {
-    static getAllBlogs() {
-        return db.blogs
+    static async getAllBlogs() {
+        const blogs = await blogCollection.find({}).toArray()
+        return blogs.map(blogMapper)
     }
 
-    static getBlogById(id: string) {
-        const blog = db.blogs.find(b => b.id === id)
+    static async getBlogById(id: string): Promise<OutputBlogType | null> {
+        const blog = await blogCollection.findOne({_id: new ObjectId(id)})
         if (!blog) {
             return null
         }
-        return blog
+        return blogMapper(blog)
     }
 
-    static createBlog(createBlog: BlogType) {
-        db.blogs.push(createBlog)
-    }
+//    static createBlog(createBlog: BlogType) {
+//        db.blogs.push(createBlog)
+//    }
 
-    static deleteBlogById(id: string) {
-        const blogIndex = db.blogs.findIndex(b => b.id === id)
-        const blog = db.blogs.find(b => b.id === id)
-
-       if(blog) {
-            db.blogs.splice(blogIndex, 1)
-            return true
-        } else {
-            return false
+    static async createBlog(newBlog: InputBlogType): Promise<BlogType> {
+        const createdBlog: BlogType = {
+            name: newBlog.name,
+            description: newBlog.description,
+            websiteUrl: newBlog.websiteUrl,
+            createdAt: new Date().toISOString(),
+            isMembership: false
         }
+        const result = await blogCollection.insertOne({ ...createdBlog });
+        createdBlog.id = result.insertedId.toString();
+        return createdBlog;
+    }
+
+    static async updateBlog(id: string, updateData: UpdateBlogData): Promise<boolean> {
+        const result = await blogCollection.updateOne(
+            {_id: new ObjectId(id)}, {
+                $set: {
+                    name: updateData.name,
+                    description: updateData.description,
+                    webisteUrl: updateData.websiteUrl,
+                },
+            }
+        )
+
+        return !!result.matchedCount
+    }
+
+    static async deleteBlogById(id: string): Promise<boolean> {
+        const  result = await blogCollection.deleteOne({_id: new ObjectId(id)})
+        return !!result.deletedCount
  }
 }
+
+//        const blogIndex = db.blogs.findIndex(b => b.id === id)
+//        const blog = db.blogs.find(b => b.id === id)
+//
+//       if(blog) {
+//            db.blogs.splice(blogIndex, 1)
+//            return true
+//            } else {
+//            return false
+//        }
--- src/repositories/post-repository.ts
+++ src/repositories/post-repository.ts
@@ -1,31 +1,75 @@
-import {db} from "../db/db";
-import {PostType} from "../types/post/output";
+import {OutputPostType, PostType} from "../types/post/output";
+import {postMapper} from "../types/post/post-mapper";
+import {postCollection} from "../db/db";
+import {ObjectId} from "mongodb";
+import {InputPostType, UpdatePostData} from "../types/post/input";
 
 export class PostRepository {
-    static getAllPosts() {
-        return db.posts
+    static async getAllPosts() {
+        const posts = await postCollection.find({}).toArray()
+        return posts.map(postMapper)
     }
 
-    static getPostById(id: string) {
-        const post = db.posts.find(p => p.id === id)
+    static async getPostById(id: string): Promise<OutputPostType | null> {
+        const post = await postCollection.findOne({_id: new ObjectId(id)})
         if (!post) {
             return null
         }
-        return post
+        return postMapper(post)
     }
 
-    static createNewPost(createdPost: PostType) {
-        db.posts.push(createdPost)
-    }
-    static deletePostById(id: string) {
-        const postIndex = db.posts.findIndex(p => p.id === id)
-        const post = db.posts.find(p => p.id === id)
+//    static createNewPost(createdPost: PostType) {
+//        db.posts.push(createdPost)
+//    }
 
-        if(post) {
-            db.posts.splice(postIndex, 1)
-            return true
-        } else {
-            return false
+    static async createNewPost(newPost: InputPostType): Promise<PostType> {
+        const createdPost: PostType = {
+            title: newPost.title,
+            shortDescription: newPost.shortDescription,
+            content: newPost.content,
+            blogId: newPost.blogId,
+            blogName: newPost.blogName,
+            createdAt: new Date().toISOString(),
         }
+        const result = await postCollection.insertOne({...createdPost});
+        createdPost.id = result.insertedId.toString();
+        return createdPost;
     }
+
+    static async updatePost(
+        id: string,
+        updateData: UpdatePostData
+    ): Promise<boolean> {
+        const result = await postCollection.updateOne(
+            {_id: new ObjectId(id)},
+            {
+                $set: {
+                    title: updateData.title,
+                    shortDescription: updateData.shortDescription,
+                    content: updateData.content,
+                    blogId: updateData.blogId,
+                },
+            }
+        );
+
+        return !!result.matchedCount;
+    }
+
+    static async deletePost(id: string): Promise<boolean> {
+        const result = await postCollection.deleteOne({ _id: new ObjectId(id) });
+
+        return !!result.deletedCount;
+    }
+
 }
+
+//    static deletePostById(id: string) {
+//        const postIndex = db.posts.findIndex(p => p.id === id)
+//        const post = db.posts.find(p => p.id === id)
+//
+//       if(post) {
+//            db.posts.splice(postIndex, 1)
+//            return true
+//        } else {
+//           return false
+//        }
