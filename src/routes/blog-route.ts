9378b1679b49dcd62d51d9849d49b37c58a11eaf
vladkharyushin <vx3110@yandex.ru>
2023-12-01 18:10:33 +0300

    Change blog-route

--- src/routes/blog-route.ts
+++ src/routes/blog-route.ts
@@ -1,21 +1,36 @@
 import {Router, Request, Response} from "express";
 import {BlogRepository} from "../repositories/blog-repository";
-import {Params, RequestWithBody, RequestWithBodyAndParams, RequestWithParams} from "../types/common";
+import {
+    Params,
+    RequestWithBody,
+    RequestWithBodyAndParams,
+    RequestWithParams,
+    RequestWithQuery,
+    SortDataType
+} from "../types/common";
 import {BlogParams} from "../types/blog/input";
 import {authMiddleware} from "../middlewares/auth/auth-middleware";
 import {blogPostValidation} from "../validators/blogs-validator";
 import {OutputBlogType} from "../types/blog/output";
 import {ObjectId} from "mongodb";
+import {BlogService} from "../domain/blog-service";
 
 export const blogRoute = Router({})
 
-blogRoute.get('/', async (req: Request, res: Response) => {
-    const blogs =  await BlogRepository.getAllBlogs()
+blogRoute.get('/', async (req: RequestWithQuery<SortDataType>, res: Response) => {
+    const sortData = {
+        searchNameTerm: req.query.searchNameTerm,
+        sortBy: req.query.sortBy,
+        sortDirection: req.query.sortDirection,
+        pageNumber: req.query.pageNumber,
+        pageSize: req.query.pageSize
+    }
+
+    const blogs =  await BlogRepository.getAllBlogs(sortData)
     res.status(200).send(blogs)
 })
 
 blogRoute.get('/:id', async (req: RequestWithParams<Params>, res: Response) => {
-    try {
         const id = req.params.id
         if(!id || !ObjectId.isValid(id)){
             res.sendStatus(404)
@@ -27,16 +42,12 @@ blogRoute.get('/:id', async (req: RequestWithParams<Params>, res: Response) => {
             return
         }
         res.status(200).send(blog)
-    } catch (e) {
-        console.log(e)
-    }
-
 })
 
 blogRoute.post('/', authMiddleware, blogPostValidation(), async (req: RequestWithBody<BlogParams>, res: Response) => {
-    const blog = await BlogRepository.createBlog(req.body);
+    const blog = await BlogService.createBlog()
 
-    return res.status(201).send(blog);
+    return res.status(201).send(blog)
 })
 
 blogRoute.put('/:id', authMiddleware, blogPostValidation(), async (req: RequestWithBodyAndParams<Params, BlogParams>, res: Response,) => {
@@ -71,19 +82,3 @@ blogRoute.delete('/:id', authMiddleware, async (req: RequestWithParams<Params>, 
     }
     return res.sendStatus(204);
 })
-
-//blogRoute.delete('/:id', authMiddleware, blogPostValidation(), (req: RequestWithParams<BlogParams>, res: Response) => {
-//    const id = req.params.id
-//    const blog = BlogRepository.getBlogById(id)
-//    if(!blog) {
-//        res.sendStatus(404)
-//        return
-//    }
-//    const  blogIndex = db.blogs.findIndex((b) => b.id == id)
-//    if(blogIndex == -1) {
-//        res.sendStatus(404)
-//        return;
-//    }
-//    db.blogs.splice(blogIndex, 1)
-//    res.sendStatus(204)
-//})
