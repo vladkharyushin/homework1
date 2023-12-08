1a33e05f24e7ad2109b7e2e9a19d016f60d29c0d
vladkharyushin <vx3110@yandex.ru>
2023-12-08 14:24:41 +0300

    Blog and post repository changes

--- src/repositories/blog-repository.ts
+++ src/repositories/blog-repository.ts
@@ -1,30 +1,10 @@
-import {BlogType, OutputBlogType} from "../types/blog/output";
+import {BlogType} from "../types/blog/output";
 import {blogCollection} from "../db/db";
 import {ObjectId} from "mongodb";
-import {blogMapper} from "../types/blog/blog-mapper";
 import {InputBlogType, UpdateBlogData} from "../types/blog/input";
-import {SortDataType} from "../types/common";
+
 
 export class BlogRepository {
-    static async getAllBlogs(sortData: SortDataType) {
-        const searchNameTerm = sortData.searchNameTerm ?? null
-        const sortBy = sortData.sortBy ?? 'cratedAt'
-        const sortDirection = sortData.sortDirection ?? 'desc'
-        const pageNumber = sortData.pageNumber ?? 1
-        const pageSize = sortData.pageSize ?? 10
-
-        const blogs = await blogCollection.find({}).toArray()
-        return blogs.map(blogMapper)
-    }
-
-    static async getBlogById(id: string): Promise<OutputBlogType | null> {
-        const blog = await blogCollection.findOne({_id: new ObjectId(id)})
-        if (!blog) {
-            return null
-        }
-        return blogMapper(blog)
-    }
-
     static async createBlog(newBlog: InputBlogType): Promise<BlogType> {
         const createdBlog: BlogType = {
             name: newBlog.name,
--- src/repositories/post-repository.ts
+++ src/repositories/post-repository.ts
@@ -1,27 +1,9 @@
-import {OutputPostType, PostType} from "../types/post/output";
-import {postMapper} from "../types/post/post-mapper";
+import {PostType} from "../types/post/output";
 import {postCollection} from "../db/db";
 import {ObjectId} from "mongodb";
 import {InputPostType, UpdatePostData} from "../types/post/input";
 
 export class PostRepository {
-    static async getAllPosts() {
-        const posts = await postCollection.find({}).toArray()
-        return posts.map(postMapper)
-    }
-
-    static async getPostById(id: string): Promise<OutputPostType | null> {
-        const post = await postCollection.findOne({_id: new ObjectId(id)})
-        if (!post) {
-            return null
-        }
-        return postMapper(post)
-    }
-
-//    static createNewPost(createdPost: PostType) {
-//        db.posts.push(createdPost)
-//    }
-
     static async createNewPost(newPost: InputPostType): Promise<PostType> {
         const createdPost: PostType = {
             title: newPost.title,
@@ -36,10 +18,7 @@ export class PostRepository {
         return createdPost;
     }
 
-    static async updatePost(
-        id: string,
-        updateData: UpdatePostData
-    ): Promise<boolean> {
+    static async updatePost(id: string, updateData: UpdatePostData): Promise<boolean> {
         const result = await postCollection.updateOne(
             {_id: new ObjectId(id)},
             {
@@ -49,8 +28,7 @@ export class PostRepository {
                     content: updateData.content,
                     blogId: updateData.blogId,
                 },
-            }
-        )
+            })
 
         return !!result.matchedCount;
     }
@@ -61,14 +39,3 @@ export class PostRepository {
         return !!result.deletedCount
     }
 }
-
-//    static deletePostById(id: string) {
-//        const postIndex = db.posts.findIndex(p => p.id === id)
-//        const post = db.posts.find(p => p.id === id)
-//
-//       if(post) {
-//            db.posts.splice(postIndex, 1)
-//            return true
-//        } else {
-//           return false
-//        }
