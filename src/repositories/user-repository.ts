a22a4cd0e8cebb7ca77a13cbc887196f08a105da
vladkharyushin <vx3110@yandex.ru>
2023-12-13 16:12:38 +0300

    Add user repository

---
+++ src/repositories/user-repository.ts
@@ -0,0 +1,21 @@
+import {InputUserType} from "../types/user/input";
+import {UserType} from "../types/user/output";
+import {userCollection} from "../db/db";
+import {ObjectId} from "mongodb";
+
+export class UserRepository {
+    static async createUser(newUser: InputUserType): Promise<UserType> {
+        const createdUser: UserType = {
+            login: newUser.login,
+            email: newUser.email,
+            createdAt: new Date().toISOString()
+        }
+        const result = await userCollection.insertOne({...createdUser})
+        createdUser.id = result.insertedId.toString()
+        return  createdUser
+    }
+    static async deleteUserById(id: string): Promise<boolean> {
+        const result = await userCollection.deleteOne({_id: new ObjectId(id)})
+        return !!result.deletedCount
+    }
+}
