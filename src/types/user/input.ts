1e7a6ad1413778db25405a3ea822347811a77960
vladkharyushin <vx3110@yandex.ru>
2023-12-13 16:11:40 +0300

    Add user types

---
+++ src/types/user/input.ts
@@ -0,0 +1,5 @@
+export type InputUserType = {
+    login: string
+    email: string
+    createdAt: string
+}
---
+++ src/types/user/output.ts
@@ -0,0 +1,22 @@
+export type UserType = {
+    id?: string
+    login: string
+    email: string
+    createdAt: string
+}
+
+export type UserSortDataType = {
+    sortBy?: string
+    sortDirection?: 'asc' | 'desc'
+    pageNumber?: number
+    pageSize?: number
+    searchLoginTerm?: string
+    searchEmailTerm?: string
+}
+
+export type OutputUserType = {
+    id: string
+    login: string
+    email: string
+    createdAt: string
+}
