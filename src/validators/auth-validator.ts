8d7f28a23b9c36ec08fde3553779c87b1744de61
vladkharyushin <vx3110@yandex.ru>
2023-12-18 17:24:52 +0300

    Fix

--- src/validators/auth-validator.ts
+++ src/validators/auth-validator.ts
@@ -2,24 +2,25 @@ import {body} from "express-validator";
 import {inputModelValidation} from "../middlewares/inputModel/input-model-validation";
 
 const loginOrEmailValidation = body('loginOrEmail')
-    .isString()
     .trim()
-    .notEmpty()
-    .custom(async (value) => {
-    if (/^[a-zA-Z0-9_-]{3, 10}$/.test(value)) {
-        return true
-    } else if (/^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/.test(value)) {
-        return true
-    } else {
-        throw new Error('Incorrect login or email')
-    }
-})
-    .withMessage('Incorrect login or email')
+    .isString()
+    .isLength({min: 3, max:50})
+//     .custom(async (value) => {
+//         console.log({value})
+//     if (/^[a-zA-Z0-9_-]{3, 10}$/.test(value)) {
+//         return true
+//     } else if (/^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/.test(value)) {
+//         return true
+//     } else {
+//         throw new Error('Incorrect login or email')
+//     }
+// })
+   .withMessage('Incorrect login or email')
 
 const passwordValidation = body('password')
-    .isString()
-    .trim()
     .isLength({min: 6, max:20})
+    .trim()
+    .isString()
     .withMessage('Incorrect password')
 
 export const authValidation = () =>
