bcd8f5fb17c570034754d770b2311a47742fe2b4
vladkharyushin <vx3110@yandex.ru>
2023-12-18 16:15:11 +0300

    changes

--- src/settings.ts
+++ src/settings.ts
@@ -1,9 +1,10 @@
-import express, {Request, Response} from "express";
+import express from "express";
 import {blogRoute} from "./routes/blog-route";
 import {postRoute} from "./routes/post-route";
 import {videoRoute} from "./routes/video-route";
 import {testingRoute} from "./routes/testing-route";
 import {userRoute} from "./routes/user-route";
+import {authRoute} from "./routes/auth-route";
 
 export const app = express()
 
@@ -14,3 +15,4 @@ app.use('/videos', videoRoute)
 app.use('/blogs', blogRoute)
 app.use('/posts', postRoute)
 app.use('/users', userRoute)
+app.use('/auth', authRoute)
