86608ee8b4ba863d35406bbde928c6d8c03d9389
vladkharyushin <vx3110@yandex.ru>
2023-12-21 12:41:26 +0300

    Auth changes

--- src/routes/auth-route.ts
+++ src/routes/auth-route.ts
@@ -2,12 +2,12 @@ import {Router, Response} from "express";
 import {authValidation} from "../validators/auth-validator";
 import {RequestWithBody} from "../types/common";
 import {AuthType} from "../types/auth/input";
-import {UserRepository} from "../repositories/user-repository";
+import {UserService} from "../domain/user-service";
 
 export const authRoute = Router({})
 
 authRoute.post('/login', authValidation(), async (req: RequestWithBody<AuthType>, res: Response) => {
-    const result = await UserRepository.checkCredentials(req.body.loginOrEmail, req.body.password)
+    const result = await UserService.checkCredentials(req.body.loginOrEmail, req.body.password)
 
     if (!result) {
         res.sendStatus(401)
