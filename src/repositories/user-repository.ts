f50710f5d20d33f5a840f82a7af85e1235d713e1
vladkharyushin <vx3110@yandex.ru>
2024-01-31 14:08:54 +0300

    user-repository new operations

--- src/repositories/user-repository.ts
+++ src/repositories/user-repository.ts
@@ -1,17 +1,37 @@
-import {InputUserType} from "../types/user/input";
-import {OutputUserType} from "../types/user/output";
+import {UserDbType} from "../types/user/output";
 import {userCollection} from "../db/db";
 import {ObjectId} from "mongodb";
-import {UserService} from "../domain/user-service";
 
 export class UserRepository {
-    static async createUser(newUser: InputUserType): Promise<OutputUserType> {
-        const user = await UserService.createUser(newUser)
-        return user
+    static async createUser(newUser: UserDbType) {
+        const user = await userCollection.insertOne({...newUser});
+        newUser._id = user.insertedId
     }
 
     static async deleteUserById(id: string): Promise<boolean> {
         const result = await userCollection.deleteOne({_id: new ObjectId(id)})
         return !!result.deletedCount
+    }
+
+    static async findByLoginOrEmail(loginOrEmail: string) {
+        return userCollection.findOne({
+            $or: [
+                {email: loginOrEmail},
+                {login: loginOrEmail}
+            ]
+        })
+    }
+
+    static async findByConfirmationCode (code: string) {
+        const user = await userCollection.findOne({'emailConfirmation.confirmationCode': code})
+        return user
+    }
+
+    static async updateConfirmation(_id: ObjectId) {
+        let result = await userCollection.updateOne(
+            {_id},
+            {$set: {'emailConfirmation.isConfirmed': true}}
+        )
+        return result.modifiedCount === 1
     }
 }
