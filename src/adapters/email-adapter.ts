c5a79ddcc5348344fa4bd7810084766946a7dcf0
vladkharyushin <vx3110@yandex.ru>
2024-01-31 14:35:16 +0300

    Fix

--- src/adapters/email-adapter.ts
+++ src/adapters/email-adapter.ts
@@ -6,19 +6,28 @@ export class EmailAdapter {
     static async sendEmail(email: string, subject: string, message: string) {
         let transport = nodemailer.createTransport({
             service: 'Mail.ru',
+            secure: false,
             auth: {
                 user: 'vx3110@mail.ru',
                 pass: MAIL_RU_PASS
             }
         })
-        let args = {
+        let sendingMail = await transport.sendMail({
             from: 'Vlad <vx3110@mail.ru>',
             to: email,
             subject: subject,
-            html: message
-        }
-        let info = await transport.sendMail(args)
-
-        return info
+            html: message,
+        })
+        return sendingMail
     }
+    //     let args = {
+    //         from: 'Vlad <vx3110@mail.ru>',
+    //         to: email,
+    //         subject: subject,
+    //         html: message
+    //     }
+    //     let info = await transport.sendMail(args)
+    //
+    //     return info
+    // }
 }
