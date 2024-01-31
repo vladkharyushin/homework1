acda3ae9c395b27f56c823a07c384721c3506035
vladkharyushin <vx3110@yandex.ru>
2024-01-31 14:08:31 +0300

    user-service new operations

--- src/domain/user-service.ts
+++ src/domain/user-service.ts
@@ -1,58 +1,63 @@
 import {InputUserType} from "../types/user/input";
-import {OutputUserType, UserDbType, UserType} from "../types/user/output";
+import {OutputUserType, UserDbType} from "../types/user/output";
 import bcrypt from "bcrypt";
-import {userCollection} from "../db/db";
-import {WithId} from "mongodb";
+import {ObjectId} from "mongodb";
+import {randomUUID} from "node:crypto";
+import {add} from "date-fns";
+import {UserRepository} from "../repositories/user-repository";
+import {userMapper} from "../types/user/user-mapper";
 
-export class UserService{
-    static async _generateHash(password: string, salt: string) {
-        const hash = await bcrypt.hash(password, salt)
+export class UserService {
+    static async _generateHash(password: string) {
+        const hash = await bcrypt.hash(password, 10)
         return hash
     }
 
     static async createUser(newUser: InputUserType): Promise<OutputUserType> {
-        const passwordSalt = await bcrypt.genSalt(10)
-        const passwordHash = await this._generateHash(newUser.password, passwordSalt)
+        const passwordHash = await this._generateHash(newUser.password)
 
-        const createdUser = {
+        const createdUser: UserDbType = {
+            _id: new ObjectId(),
             login: newUser.login,
             email: newUser.email,
-            createdAt: new Date().toISOString(),
+            createdAt: new Date(),
             passwordHash,
-            passwordSalt
+            emailConfirmation: {
+                confirmationCode: randomUUID(),
+                expirationDate: add(new Date(), {
+                    hours: 1,
+                    minutes: 30
+                }),
+                isConfirmed: false
+            }
         }
+        await UserRepository.createUser(createdUser)
 
-        const result = await userCollection.insertOne({...createdUser})
-
-        return {
-            login: newUser.login,
-            email: newUser.email,
-            createdAt: createdUser.createdAt,
-            id: result.insertedId.toString(),
-        }
+        return userMapper(createdUser)
     }
 
-    static async findByLoginOrEmail(loginOrEmail: string) {
-        return userCollection.findOne({
-            $or: [
-                {email: loginOrEmail},
-                {login: loginOrEmail}
-            ]
-        })
-    }
 
-    static async checkCredentials(loginOrEmail: string, password: string): Promise<WithId<UserDbType> | null> {
-        const user = await UserService.findByLoginOrEmail(loginOrEmail);
+    static async checkCredentials(loginOrEmail: string, password: string): Promise<UserDbType | null> {
+        const user = await UserRepository.findByLoginOrEmail(loginOrEmail)
 
         if (!user)
             return null
 
-        const passwordHash = await this._generateHash(password, user.passwordSalt)
+        if (!user.emailConfirmation.isConfirmed) {
+            return null
+        }
+
+        const passwordHash = await this._generateHash(password)
 
         if (user.passwordHash !== passwordHash) {
             return null
         }
 
         return user
+    }
+
+    static async findUserByConfirmationCode(code: string) {
+        const foundedUser = await UserRepository.findByConfirmationCode(code)
+        return foundedUser
     }
 }
