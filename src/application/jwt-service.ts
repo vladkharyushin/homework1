312a883a8d3575c0b6c3ed46224d59bbe69f7e2a
vladkharyushin <vx3110@yandex.ru>
2024-04-09 13:31:15 +0300

    Add refresh token

--- src/application/jwt-service.ts
+++ src/application/jwt-service.ts
@@ -1,18 +1,33 @@
-import jwt from "jsonwebtoken";
+import jwt, {JwtPayload} from "jsonwebtoken";
 import {JWT_SECRET} from "../config";
 
 export class jwtService {
-    static async createJWT(userId: string) {
-        const accessToken = jwt.sign({userId}, JWT_SECRET, {expiresIn: "1h"})
+    static async createAccessToken(userId: string) {
+        const accessToken = jwt.sign({userId}, JWT_SECRET, {expiresIn: "10000"})
         return accessToken
     }
+
+    static async createRefreshToken(userId: string) {
+        const refreshToken = jwt.sign({userId}, JWT_SECRET, {expiresIn: "20000"})
+        return refreshToken
+    }
+
     static async getUserIdByToken(accessToken: string) {
         try {
             const result: any = jwt.verify(accessToken, JWT_SECRET)
             return result.userId
         } catch (error) {
-            console.log(error,'error in verify')
+            console.log(error, 'error in verify')
             return null
         }
+    }
+
+    static async decodeToken(token: string) {
+        try {
+            return jwt.verify(token, JWT_SECRET) as JwtPayload
+        } catch (e) {
+            return null
+        }
+
     }
 }
